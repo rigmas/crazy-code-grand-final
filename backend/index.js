cb81b61dd4f84c076eabeb8775f4717d063aec03
Samuel Girsang <samuel.pangidoan@traveloka.com>
2024-10-20 23:22:34 +0700

    initialize QR generator and scanner

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,31 @@
 require('dotenv').config();
 
 const express = require('express');
+const QRCode = require('qrcode');
 
 const app = express();
 const port = process.env.PORT || 8989;
 
 app.get('/', (req, res) => {
   res.send('Test, samuel!');
+});
+
+app.get('/qr/generate', async (req, res) => {
+  try {
+    const { text } = req.query;
+    
+    // Generate QR code data as a PNG image
+    const qrCodeDataUrl = await QRCode.toDataURL(text || '1');
+
+    console.log({ qrCodeDataUrl })
+
+    res.json({
+      qrCodeUrl: qrCodeDataUrl
+    });
+  } catch (error) {
+    console.log({ error })
+    res.status(500).json({ error: 'Failed to generate QR code' });
+  }
 });
 
 app.listen(port, () => {
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        <h1>Welcome to the homepage</h1>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <div class="flex">
+      <button>
+        <a href="/qr">qr</a>
+      </button>
+
+      <button>
+        <a href="/profile">profile</a>
+      </button>
+      
     </div>
+  </div>
 </template>
