0a1e138fa65b776d5bb11b148bf979960bc74057
rigmas <samueelgrs@gmail.com>
2024-10-26 12:59:43 +0700

    get quests object to list response

--- backend/repositories/quest.js
+++ backend/repositories/quest.js
@@ -3,7 +3,7 @@ const db = require(".");
 exports.getQuests = async () => {
   try {
     const res = await db.query("SELECT * FROM quests");
-    return res.rows[0];
+    return res.rows;
   } catch (err) {
     throw err;
   }
