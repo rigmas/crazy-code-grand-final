2da047932ae8d21c256d5d6b519977fe1961d823
rigmas <samueelgrs@gmail.com>
2024-10-26 10:33:25 +0700

    update seeder user quest

--- backend/db/seed-table/3-seed-quests.sql
+++ backend/db/seed-table/3-seed-quests.sql
@@ -1,3 +1,3 @@
--- INSERT INTO quests (id, type, title, reward, is_party, description, objectives, mind_file, lon_lat) VALUES (1, 'photo', 'test quest photo A', 1000, FALSE, 'description testing', ARRAY['objective 1', 'objective 2'], NULL, '(41.8781, -87.6298)');
+INSERT INTO quests (id, type, title, reward, is_party, description, objectives, mind_file, lon_lat) VALUES (1, 'photo', 'test quest photo A', 1000, FALSE, 'description testing', ARRAY['objective 1', 'objective 2'], NULL, '(41.8781, -87.6298)');
 
-INSERT INTO user_quests (id, user_id, quest_id) VALUES (1, 1, 1);
+-- INSERT INTO user_quests (id, user_id, quest_id) VALUES (1, 1, 1);
