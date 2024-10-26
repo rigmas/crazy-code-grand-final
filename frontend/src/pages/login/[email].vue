97f9cd45ad540faf23286ff453516626e3211b45
luthfanadli <luthfan30@gmail.com>
2024-10-26 16:18:29 +0700

    feat: login using scan qr, set auth store

---
+++ frontend/src/pages/login/[email].vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from "vue-router"
+import { profileUsers } from "~/services/user"
+import { useAuthStore } from "~/stores/auth.store"
+
+interface RouteParams {
+  email: string
+}
+
+const route = useRoute()
+const router = useRouter()
+const path = route.params as RouteParams
+const authStore = useAuthStore()
+
+function navigate() {
+  router.push("/welcome")
+}
+
+onMounted(async () => {
+  const res = await profileUsers(path.email)
+  if (res?.data) {
+    authStore.setUser(res.data)
+  }
+})
+</script>
+
+<template>
+  <div class="flex justify-center p-8 flex-col text-center gap-10 items-center">
+    <div class="text-2xl text-primary font-semibold mt-8">Welcome to Lyf!</div>
+    <img src="/boy-toss.png" alt="welcomeIMG" class="h-[270px] w-[270px] " />
+    <div class="text-lg mb-10"> Jump into a fun adventure! Make friends, grab cool gifts, and take on exciting
+      quests as
+      you
+      explore all
+      around you.</div>
+    <van-button size="large" type="primary" @click="navigate">
+      Let's Go!
+    </van-button>
+    <img class="mt-15 h-[34px] w-[35px]" src="/lyf-logo-orange.png">
+  </div>
+</template>
--- frontend/src/stores/auth.store.ts
+++ frontend/src/stores/auth.store.ts
@@ -1,7 +1,14 @@
+import { useStorage } from "@vueuse/core"
 import { defineStore } from "pinia"
+import type { User } from "~/schemas/user"
 
 export const useAuthStore = defineStore("authStore", () => {
   const authToken = useStorage<string | undefined>("token", undefined)
+  const user = ref<User | null>(null)
 
-  return { auth: authToken }
+  function setUser(userData: User) {
+    user.value = userData
+  }
+
+  return { auth: authToken, user, setUser }
 })
