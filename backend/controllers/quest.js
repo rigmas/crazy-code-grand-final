673c983cb30153fd469c0e9660833c38390cdce6
rigmas <samueelgrs@gmail.com>
2024-10-26 10:52:12 +0700

    refactor quest to user controller and repo

--- backend/controllers/quest.js
+++ backend/controllers/quest.js
@@ -9,36 +9,3 @@ exports.getAllQuests = async () => {
     throw err;
   }
 }
-
-exports.addNewUserQuest = async (req) => {
-  const { quest_id, user_id } = req.params
-  try {
-    const userQuest = await questRepo.insertUserQuest(user_id, quest_id, false);
-    return { message: "Add new user quest", data: userQuest };
-  } catch (err) {
-    console.error("quest::addNewUserQuest - err: ", err);
-    throw err;
-  }
-}
-
-exports.completeUserQuest = async (req) => {
-  const { quest_id, user_id } = req.params
-  try {
-    const userQuest = await questRepo.completeUserQuest(user_id, quest_id, true);
-    return { message: "Complete user quest", data: userQuest };
-  } catch (err) {
-    console.error("quest::completeUserQuest - err: ", err);
-    throw err;
-  }
-}
-
-exports.getUserQuestXp = async (req) => {
-  const { user_id } = req.params
-  try {
-    const userQuest = await questRepo.getTotalXpUserQuests(user_id);
-    return { message: "Get total xp user quests", data: userQuest };
-  } catch (err) {
-    console.error("quest::getUserQuestXp - err: ", err);
-    throw err;
-  }
-}
--- backend/repositories/user.js
+++ backend/repositories/user.js
@@ -23,3 +23,78 @@ exports.updatePersonalityByEmail = async (personality, userId) => {
     throw err;
   }
 };
+
+exports.insertUser = async (name, email, roomNumber, checkInDate, checkoutDate) => {
+  try {
+    const res = await db.query(
+      `
+      INSERT INTO users (name, email, room_number, check_in_date, check_out_date)
+      VALUES ($1, $2, $3, $4, $5) RETURNING *
+      `,
+      [name, email, roomNumber, checkInDate, checkoutDate]
+    );
+    return res.rows[0];
+  } catch (err) {
+    throw err;
+  }
+}
+
+exports.insertUserQuest = async (userId, questId, isDone) => {
+  try {
+    const res = await db.query(
+      `
+      INSERT INTO user_quests (user_id, quest_id, done)
+      VALUES ($1, $2, $3)
+      RETURNING *
+      `,
+      [userId, questId, isDone]
+    );
+    return res.rows[0];
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.completeUserQuest = async (userId, questId, isDone) => {
+  try {
+    const res = await db.query(
+      `
+      UPDATE user_quests SET done = $1
+      WHERE user_id = $2 AND quest_id = $3
+      RETURNING *
+      `,
+      [isDone, userId, questId]
+    );
+    return res.rows[0];
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.getTotalXpUserQuests = async (userId) => {
+  try {
+    const res = await db.query(
+      `
+      SELECT 
+      u.id AS user_id,
+      u.name,
+      u.email,
+      COALESCE(SUM(CASE WHEN uq.done THEN CAST(q.reward AS INT) ELSE 0 END), 0) AS xp
+      FROM 
+        users u
+      JOIN 
+        user_quests uq ON u.id = uq.user_id
+      JOIN 
+        quests q ON uq.quest_id = q.id
+      WHERE 
+        u.id = $1
+      GROUP BY 
+        u.id;
+      `,
+      [userId]
+    );
+    return res.rows[0];
+  } catch (err) {
+    throw err;
+  }
+}
